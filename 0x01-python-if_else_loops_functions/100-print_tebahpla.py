b219e82aa08cbb1012a69fd0b4aacfd6f939587f
antoineleno <johndoe@example.com>
2024-03-12 19:03:59 +0800

    Python if, else and function

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
-for i in range(122, 96, -1):
-    print(chr(i if i % 2 == 0 else i - 32), end = ("" if i != 97 else "\n"))
 
+#!/usr/bin/python3
+for i in range(ord('z'), ord('a') -1, -1):
+    print("{:c}".format(i), end="")
