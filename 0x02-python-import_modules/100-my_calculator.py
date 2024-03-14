e44dc7392075a6721c0df3143a6b78edd12f0e99
antoineleno <johndoe@example.com>
2024-03-15 01:30:36 +0800

    Python import module

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -11,13 +11,13 @@ if __name__ == "__main__":
         operator = sys.argv[2]
         a, b = int(sys.argv[1]), int(sys.argv[3])
         if operator ==  "+":
-                print("{} + {} = {}".format(a, b, add(a, b)))
+            print("{} + {} = {}".format(a, b, add(a, b)))
         elif operator == "-":
-                print("{} + {} = {}".format(a, b, sub(a, b)))
+            print("{} + {} = {}".format(a, b, sub(a, b)))
         elif operator ==  "*":
-                print("{} + {} = {}".format(a, b, mul(a, b)))
+            print("{} + {} = {}".format(a, b, mul(a, b)))
         elif operator ==  "/":
-                print("{} + {} = {}".format(a, b, div(a, b)))
-        else :
-                print("Unknown operator. Available operators: +, -, * and /")
-                sys.exit(1)
+             print("{} + {} = {}".format(a, b, div(a, b)))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            sys.exit(1)
