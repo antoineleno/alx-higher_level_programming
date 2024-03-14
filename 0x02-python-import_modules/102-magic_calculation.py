b52493ba04b1259d78fd285b787ed6616816c29f
antoineleno <johndoe@example.com>
2024-03-15 01:52:36 +0800

    Python impport module

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-import dis
 def magic_calculation(a, b):
-    add = __import__('magic_calculation_102').add
-    sub = __import__('magic_calculation_102').sub
+    add, sub = __import__('magic_calculation_102').add, __import__('magic_calculation_102').sub
 
     if a < b:
         c = add(a, b)
@@ -11,6 +9,3 @@ def magic_calculation(a, b):
         return c
     else:
         return sub(a, b)
-
-
-print(dis.dis(magic_calculation))
