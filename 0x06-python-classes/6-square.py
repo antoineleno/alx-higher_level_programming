e8e64b3be335a6750ede234c1c9ebd5f72eef5f0
antoineleno <johndoe@example.com>
2024-04-02 22:38:07 +0800

    Python classes

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -60,9 +60,10 @@ class Square:
                 Raise the Type error if the value is not an interger
                 Raise the Value error if the value is less than 0
         """
-        if len(value) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif value[0] < 0 or value[1] < 0:
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
     """Function area that return the square area"""
