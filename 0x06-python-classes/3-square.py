e367e0d8d9aa36f23b32dac44a94c618c10d612e
antoineleno <johndoe@example.com>
2024-04-02 21:43:15 +0800

    Python classes

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,13 +1,16 @@
+#!/usr/bin/python3
 """Definiton of a class Square that defines
 
     a square by: (based on 2-square.py)
 """
+
+
 class Square:
     """A class square
-    
+
         Args:
             size (int): Size to calculate the square
-            
+
         Return: The square area
     """
     def __init__(self, size=0):
@@ -17,12 +20,12 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
-        
+
     def area(self):
         """A function name are
-            
+
             Args: The size
-            
+
             Return: the square area
         """
         return self.__size**2
