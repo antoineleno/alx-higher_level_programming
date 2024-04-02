d16304ea45e7d11e9029333e2c9e6df89bc8a71c
antoineleno <johndoe@example.com>
2024-04-02 23:00:16 +0800

    Python classes

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -3,12 +3,14 @@
 
     a square by: (based on 4-square.py)
 """
+
+
 class Square:
     """A class square
-    
+
         Args:
             size (int): Size to calculate the square
-            
+
         Return: The square area
     """
     def __init__(self, size=0):
@@ -18,9 +20,10 @@ class Square:
     def size(self):
         """Function size to return the value of size"""
         return self.__size
+
     @size.setter
     def size(self, value):
-        """Function size setter to set the value of size 
+        """Function size setter to set the value of size
             base on certains conditions
 
             Args :
@@ -41,21 +44,25 @@ class Square:
     """Function areas Function that return the square area"""
     def area(self):
         return self.__size**2
+
     def __eq__(self, other):
         """comparaison method for equality"""
         if isinstance(other, Square):
             return self.area() == other.area()
         return NotImplemented
+
     def __ne__(self, other):
         """comparaison method for not equal"""
         if isinstance(other, Square):
             return self.area() != other.area()
         return NotImplemented
+
     def __lt__(self, other):
         """comparaison method for less than"""
         if isinstance(other, Square):
             return self.area() < other.area()
         return NotImplemented
+
     def __le__(self, other):
         """comparaison method for less than or equal"""
         if isinstance(other, Square):
@@ -71,4 +78,3 @@ class Square:
         if isinstance(other, Square):
             return self.area() >= other.area()
         return NotImplemented
-    
