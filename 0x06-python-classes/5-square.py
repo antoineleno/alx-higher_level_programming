d63debb1c15ea807ee4ce36b4dba2046fc72fbba
antoineleno <johndoe@example.com>
2024-04-02 22:00:07 +0800

    Python classes

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -44,7 +44,10 @@ class Square:
         return self.__size**2
     """Function my_print to print in stdout the square with the character #"""
     def my_print(self):
-        for i in range(self.__size):
-            for j in range(self.__size):
-                print("#", end="")
+        if self.__size == 0:
             print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
