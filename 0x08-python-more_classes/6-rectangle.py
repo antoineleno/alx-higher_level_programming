1ead8aa367c92904c0e424e6cbecb4bfee1c327f
antoineleno <johndoe@example.com>
2024-04-09 17:59:10 +0800

    Python more classes

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -3,28 +3,33 @@
 
     a rectangle by: (based on 5-rectangle.py)
 """
+
+
 class Rectangle:
     """A class rectangle
-    
         Args:
             width (int): width of the rectangle
-            height (int): heigh of the rectangle 
+            height (int): heigh of the rectangle
         Raise:
             Type error and Value error
         Return: The rectangle area
     """
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.__height = height
         self.__width = width
         Rectangle.number_of_instances += 1
+
     def __del__(self):
-        print ("Bye rectangle...")
-        Rectangle.number_of_instances -=1
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
+
     @property
     def width(self):
         """A private instance attribute width"""
         return self.__width
+
     @width.setter
     def width(self, value):
         """Propoerty setter width
@@ -44,12 +49,12 @@ class Rectangle:
     def height(self):
         """A private instance attribute width"""
         return self.__height
+
     @height.setter
     def height(self, value):
         """Propoerty setter width
             Args:
                 value (int): Value to be used
-                
             Return:
                     The width
         """
@@ -58,16 +63,19 @@ class Rectangle:
         elif value < 0:
             raise ValueError("height must be >= 0")
         else:
-            self.__height = value          
+            self.__height = value
+
     def area(self):
         """area function to return the area"""
         return self.__height * self.__width
+
     def perimeter(self):
         """perimeter function to return the perimeter"""
         if self.__width == 0 or self.__height == 0:
             return 0
         else:
             return 2*(self.__height + self.__width)
+
     def __str__(self):
         """string method to represente a rectangle"""
         if self.__width == 0 or self.__height == 0:
@@ -76,6 +84,6 @@ class Rectangle:
         for _ in range(self.__height):
             rect_str += '#' * self.__width + '\n'
         return rect_str.rstrip()
+
     def __repr__(self):
         return f"Rectangle({self.__width}, {self.__height})"
-    
