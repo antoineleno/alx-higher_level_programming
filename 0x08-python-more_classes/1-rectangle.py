9356101ae13ca2fe77a7215b9d905415e91101f9
antoineleno <johndoe@example.com>
2024-04-09 17:31:50 +0800

    Python more classes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -51,7 +51,7 @@ class Rectangle:
             Return:
                     The width
         """
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
