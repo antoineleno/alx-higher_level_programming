742a998c325fcd4691fa09caf2196bb33092ca5c
antoineleno <johndoe@example.com>
2024-04-16 02:36:21 +0800

    Python inheritance

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -3,8 +3,29 @@
 
 
 class MyInt(int):
+    """my_int_class
+
+    Args:
+        int (_type_): _description_
+    """
     def __eq__(self, other):
+        """_summary_
+
+        Args:
+            other (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
         return super().__ne__(other)
 
     def __ne__(self, other):
+        """_summary_
+
+        Args:
+            other (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
         return super().__eq__(other)
