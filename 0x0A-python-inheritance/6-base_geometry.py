fd4433bf7d2117d878ecb6c049ca601ecf2bfdf0
antoineleno <johndoe@example.com>
2024-04-16 02:25:38 +0800

    Python inheritance

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """BaseGeometroy"""
-class  BaseGeometry:
+
+
+class BaseGeometry:
     """BaseGeometry: function that defien an empty class
     """
     def area(self):
         """area: Function to calculate the area
-
         Raises:
             Exception: exception with the message area() is not implemented
-        """        
-        raise Exception ("area() is not implemented")
+        """
+        raise Exception("area() is not implemented")
