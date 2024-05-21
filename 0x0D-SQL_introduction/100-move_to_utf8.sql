45e1ff0ab8e47556dfa9c02a7fcfae0e8364e6ce
antoineleno <johndoe@example.com>
2024-05-22 01:36:22 +0800

    SQL introduction

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,7 @@
 -- Change the database, table and column charset
+-- table
+-- column
+
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 ALTER first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
