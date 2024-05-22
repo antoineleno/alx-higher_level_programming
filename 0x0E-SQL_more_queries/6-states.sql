cbf89d4f82857ecaf87544717a43e369a564894c
antoineleno <johndoe@example.com>
2024-05-22 19:16:25 +0800

    SQL more queries

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,5 @@
 -- Creates the database hbtn_0d_usa and table states
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (id INT NOT NULL AUTO INCREMENT UNIQUE, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (id INT NOT NULL AUTO INCREMENT UNIQUE, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
 
