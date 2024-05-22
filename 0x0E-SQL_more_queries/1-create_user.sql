b68aa2e5148ab8ec2a9ee551b6ba123b9cdad36e
antoineleno <johndoe@example.com>
2024-05-22 18:33:30 +0800

    0X03-SQL more queries

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- Create users user_0d_1 with the password user_0d_1_pwd and grant him all privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
+
