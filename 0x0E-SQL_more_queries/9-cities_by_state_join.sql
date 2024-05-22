3816383bdfb6a1c56753d4eac07300a50777d6bc
antoineleno <johndoe@example.com>
2024-05-22 19:40:53 +0800

    sql more queries

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,7 @@
 -- Lists all cities contained in the database hbtn_0d_usa
-SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California');
+SELECT c.`id`, c.`name`, s.`name`
+  FROM `cities` AS c
+       INNER JOIN `states` AS s
+       ON c.`state_id` = s.`id`
+ ORDER BY c.`id`;
 
