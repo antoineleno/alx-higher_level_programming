d1f6e04e99c8fda59bbfed7019c7cd5ee689583d
antoineleno <johndoe@example.com>
2024-05-22 19:44:46 +0800

    sql more queries

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,7 @@
 -- Lists all cities contained in the database hbtn_0d_usa
 SELECT cities.id, cities.name AS city_name, states.name AS state_name
 FROM cities
-JOIN states ON cities.state_id = states.id
+INNER JOIN states ON cities.state_id = states.id
 ORDER BY cities.id;
 
 
