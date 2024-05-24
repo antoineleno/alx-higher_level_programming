1cafec6c6622c0a7ae5d1c108ab15108fd7fde7a
antoineleno <johndoe@example.com>
2024-05-24 19:16:05 +0800

    SQL more queries

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,10 @@
+-- Lists all shows contained in the database hbtn_0d_tvshows.
+-- Displays NULL for shows without genres.
+-- Records are ordered by ascending tv_shows.title and tv_show_genres.genre_id.
+SELECT genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM genres
+JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
+GROUP BY genres.name
+HAVING number_of_shows > 0
+ORDER BY number_of_shows DESC;
+
