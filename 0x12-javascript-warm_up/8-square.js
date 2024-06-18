1c272a4de9a9290418a5bb3d5aa401abc879a953
antoineleno <johndoe@example.com>
2024-06-18 19:28:31 +0800

    js warm up

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const size = process.argv[2];
+
+const sizeInt = parseInt(size);
+
+if (!isNaN(sizeInt)) {
+
+  for (let i = 0; i < sizeInt; i++) {
+    let row = '';
+    for (let j = 0; j < sizeInt; j++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
+} else {
+  console.log("Missing size");
+}
