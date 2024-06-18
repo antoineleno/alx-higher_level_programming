49327de0e988d5102903c828cc50e6f323652b27
antoineleno <johndoe@example.com>
2024-06-18 19:07:57 +0800

    js warm up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const myVariables = ['C is fun', 'Python is coll', 'JavaScript is amazing'];
+const myVariables = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < 3; i++) {
   console.log(myVariables[i]);
 }
