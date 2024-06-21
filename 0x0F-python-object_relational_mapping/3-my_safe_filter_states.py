4049cc0e6ff5effd69160f961c18cf43f719c483
antoineleno <johndoe@example.com>
2024-06-22 00:12:31 +0800

    ORM

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+3-My safe filter states modeule
+"""
+import MySQLdb
+import sys
+
+
+def list_all_states(username, db_password, db_name, state_searched):
+    """list_all_states function
+
+    Args:
+        username (string): The username of the database
+        db_password (String): Database password
+        db_name (String): Database name
+        state_searched(String): State to search
+    """
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=username,
+                         password=db_password,
+                         db=db_name)
+
+    cursor = db.cursor()
+    query = "SELECT * FROM states WHERE name=%s"
+    cursor.execute(query, (state_searched,))
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
+        cursor.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: {} <user> <pwd> <db> <state>".format(sys.argv[0]))
+        sys.exit(1)
+    else:
+        list_all_states(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
