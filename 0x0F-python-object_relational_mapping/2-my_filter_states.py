0185aa5c897d204091166eb7c84fb6a3c48a936c
antoineleno <johndoe@example.com>
2024-06-21 18:16:03 +0800

    ORM

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,13 +6,14 @@ import MySQLdb
 import sys
 
 
-def list_all_states(username, db_password, db_name):
+def list_all_states(username, db_password, db_name, state_searched):
     """list_all_states function
 
     Args:
         username (string): The username of the database
         db_password (String): Database password
         db_name (String): Database name
+        state_searched(String): State to search
     """
     db = MySQLdb.connect(host="localhost",
                          port=3306,
@@ -21,9 +22,7 @@ def list_all_states(username, db_password, db_name):
                          db=db_name)
 
     cursor = db.cursor()
-    MY_query = ("SELECT * FROM states WHERE "
-                "name COLLATE utf8mb4_bin LIKE 'C%' ORDER BY states.id ASC")
-    cursor.execute(MY_query)
+    cursor.execute(f'SELECT * FROM states WHERE name="{state_searched}"')
     result = cursor.fetchall()
     for row in result:
         print(row)
@@ -31,8 +30,8 @@ def list_all_states(username, db_password, db_name):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+    if len(sys.argv) != 5:
+        print("Usage: {} <user> <pwd> <db> <state>".format(sys.argv[0]))
         sys.exit(1)
     else:
-        list_all_states(sys.argv[1], sys.argv[2], sys.argv[3])
+        list_all_states(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
