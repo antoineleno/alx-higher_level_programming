b39eea2729ca21c17b971e5ac6a5a5bbcde687c6
antoineleno <johndoe@example.com>
2024-06-21 17:33:16 +0800

    ORM

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,18 +1,38 @@
 #!/usr/bin/python3
-import MySQLdb
 """
 0-Select states modeule
 """
+import MySQLdb
+import sys
 
-db = MySQLdb.connect(host="localhost", port=3306,db="hbtn_0e_0_usa")
-cursor = db.cursor()
-cursor.execute("SELECT * FROM ANTIOO")
 
-result = cursor.fetchall()
-for row in result:
+def list_all_states(username, db_password, db_name):
+    """list_all_states function
+
+    Args:
+        username (string): The username of the database
+        db_password (String): Database password
+        db_name (String): Database name
+    """
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=username,
+                         password=db_password,
+                         db=db_name)
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    result = cursor.fetchall()
+    for row in result:
         print(row)
 
+        cursor.close()
+        db.close()
 
-cursor.close()
-db.close()
 
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+    else:
+        list_all_states(sys.argv[1], sys.argv[2], sys.argv[3])
