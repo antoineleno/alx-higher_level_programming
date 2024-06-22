cc9cebd229b03595c4afdd5d6e6314ff654ad019
antoineleno <johndoe@example.com>
2024-06-23 04:14:14 +0800

    ORM

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,20 +1,37 @@
 #!/usr/bin/python3
-"""List all states"""
-from sys import argv
-from model_state import Base, State
-from sqlalchemy import (create_engine)
+"""
+model_state module
+"""
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base, State
+import sys
 
-if __name__ == "__main__":
+
+def list_all_states(username, db_password, db_name):
+    """list_all_states
+
+    Args:
+        username (string): _description_
+        db_password (string): db password
+        db_name (string): db name
+    """
+
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost:3306/{}'
-        .format(argv[1], argv[2],
-                argv[3]), pool_pre_ping=True)
+        .format(username, db_password,
+                db_name), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    session = sessionmaker(bind=engine)
+    session = session()
     states = session.query(State).filter(State.name.like('%a%')).all()
+
     for state in states:
         session.delete(state)
     session.commit()
     session.close()
+
+
+if __name__ == "__main__":
+    list_all_states(sys.argv[1], sys.argv[2], sys.argv[3])
