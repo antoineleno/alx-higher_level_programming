6389e4146483516db78cdf43cc35faee0f5eaae9
antoineleno <johndoe@example.com>
2024-06-23 02:32:43 +0800

    ORM

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,21 +1,43 @@
 #!/usr/bin/python3
-"""List all states"""
-from sys import argv
-from model_state import Base, State
-from sqlalchemy import (create_engine)
+"""
+model_state module
+"""
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base, State
+import sys
 
-if __name__ == "__main__":
+
+def list_all_states(username, db_password, db_name):
+    """list_all_states
+
+    Args:
+        username (string): _description_
+        db_password (string): db password
+        db_name (string): db name
+    """
+
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost/{}'
-        .format(argv[1], argv[2],
-                argv[3]), pool_pre_ping=True)
+        .format(username, db_password,
+                db_name), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    state = session.query(State).order_by(State.id).first()
-    if state:
-        print("{}: {}".format(state.id, state.name))
+    session = sessionmaker(bind=engine)
+    session = session()
+    states = session.query(State).order_by(State.id).first()
+    if states:
+        for state in states:
+                print("{}: {}".format(state.id, state.name))
     else:
         print("Nothing")
     session.close()
+
+
+if __name__ == "__main__":
+    if (len(sys.argv) != 4):
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+    else:
+        list_all_states(sys.argv[1], sys.argv[2], sys.argv[3])
