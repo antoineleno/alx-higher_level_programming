dbc2de693c8cfb650c14bb85cbfb11b8d91174ff
antoineleno <johndoe@example.com>
2024-06-23 04:34:38 +0800

    ORM

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -27,6 +27,7 @@ def list_all_states(username, db_password, db_name, state_searched):
     for row in result:
         print(row)
         cursor.close()
+        db.close()
 
 
 if __name__ == "__main__":
