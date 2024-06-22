1f399092579199a373265bb058fab16029ec773b
antoineleno <johndoe@example.com>
2024-06-23 02:05:04 +0800

    ORM

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,36 +1,18 @@
 #!/usr/bin/python3
-"""
-model_state module
-"""
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+"""List all states"""
+from sys import argv
 from model_state import Base, State
-import sys
-
-def list_all_states(username, db_password, db_name):
-    """list_all_states 
-
-    Args:
-        username (string): _description_
-        db_password (string): db password
-        db_name (string): db name
-    """
-    
-    db_url = f"mysql+mysqlconnector://{username}:{db_password}@localhost:3306/{db_name}"
-    engine = create_engine(db_url,echo=True)
-    Base.metadata.create_all(engine)
-    session = sessionmaker(bind=engine)
-    session = session()
-    states = session.query(State).order_by(State.id).all()
-    for state in states:
-        print(state)
-
-    
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    if (len(sys.argv) != 4):
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
-    else:
-        list_all_states(sys.argv[1], sys.argv[2], sys.argv[3])
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(argv[1], argv[2],
+                argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+    session.close()
