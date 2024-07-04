2ce253068b161f14c56102e619c884b342c30547
antoineleno <johndoe@example.com>
2024-07-05 02:11:28 +0800

    python network

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,16 +1,39 @@
 #!/usr/bin/python3
+"""6-peak module
+"""
+
 
 def find_peak(list_of_integers):
-    final_result = []
-    for i in range(len(list_of_integers)):
-        if i == 0:
-            pass
-        else:
-            if (list_of_integers[i] > list_of_integers[i - 1] and list_of_integers[i] > list_of_integers[i + 1]):
-                final_result.append(list_of_integers[i])
+    """find_peak: Function to find the peak of a list
 
-    my_len = len(final_result)
-    if my_len > 0:
-        return final_result[0]
-    else:
+    Args:
+        list_of_integers (list): List to find a peak from it
+
+    Returns:
+        The peak number
+    """
+    if not list_of_integers:
         return None
+
+    def binary_search_peak(nums, low, high):
+        """binary search peak
+
+        Args:
+            nums (list): list of integers
+            low (int): lowest number
+            high (int): Highest number
+
+        Returns:
+            peak value: The peak  value
+        """
+        if low == high:
+            return nums[low]
+
+        mid = (low + high) // 2
+
+        if nums[mid] < nums[mid + 1]:
+            return binary_search_peak(nums, mid + 1, high)
+        else:
+            return binary_search_peak(nums, low, mid)
+
+    return binary_search_peak(list_of_integers, 0, len(list_of_integers) - 1)
