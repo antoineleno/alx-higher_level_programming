ab9804dd9d31de871a3835df844c76b02ee2188e
antoineleno <johndoe@example.com>
2024-07-05 01:01:44 +0800

    python network

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that send a get request to url
-curl -s -H "X-School-User-Id: 98" "$1"
+curl -sH "X-School-User-Id: 98" "$1"
---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Bash script that send a post request to url
+curl -s -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$URL"
