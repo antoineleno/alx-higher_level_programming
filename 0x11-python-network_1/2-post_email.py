d61e6ac5bee6fad8f6102254e2623510433dffcb
antoineleno <johndoe@example.com>
2024-07-05 21:41:12 +0800

    python network

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""hbtn_header module
+"""
+
+import urllib.request
+import urllib.parse
+import sys
+
+url = sys.argv[1]
+email = sys.argv[2]
+
+data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+
+with urllib.request.urlopen(url, data=data) as response:
+    body = response.read()
+    print(body.decode('utf-8'))
