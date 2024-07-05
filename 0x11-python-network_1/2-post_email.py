e17832dddaace29f90c7b0fb8018c3c4c29bd94c
antoineleno <johndoe@example.com>
2024-07-05 21:43:35 +0800

    python network

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,13 +3,12 @@
 """
 
 import urllib.request
-import urllib.parse
 import sys
 
 url = sys.argv[1]
 email = sys.argv[2]
 
-data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+data = f"email={email}".encode('utf-8')
 
 with urllib.request.urlopen(url, data=data) as response:
     body = response.read()
